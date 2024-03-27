9cc628b8da76bc86b0e43052eb4c7f9d4c8f2922
username9896 <103965166+username9896@users.noreply.github.com>
2024-03-27 13:10:38 +1100

    Initial commit

---
+++ eece5560/packages/controls_hw/src/vehicle_dynamics.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import rospy
+from numpy import arange,sign
+from random import random
+from std_msgs.msg import Float32
+
+class VehicleDynamics:
+    def __init__(self, m, mu, g, area, c, rho, v0, p0, noise_mag):
+        self.m = m # mass, kg
+        self.mu = mu # friction coefficient
+        self.g = g # gravity, m/s^2
+        self.area = area # cross section, m^2
+        self.c = c # drag coefficient
+        self.rho = rho # air density, kg/m^3
+        self.xd = v0 # initial velocity, m/s
+        self.x = p0 # initial position, m
+        self.noise_mag = noise_mag # magnitude of noise
+        self.control = 0 # control input, m/s^2
+        
+    def iterate(self, dt):
+        # a = engine acceleration - friction - drag
+        self.xdd = self.control - \
+            sign(self.xd)*(self.mu*self.g + \
+                (self.c*self.rho*self.area*self.xd*self.xd)/(2*self.m)) #+ \
+#            self.noise_mag*random()
+        self.xd += self.xdd*dt
+        self.x += self.xd*dt
+        return self.x, self.xd, self.xdd
+    def update_control(self, control):
+        self.control = control.data
+
+if __name__ == '__main__':
+    try:
+        desired = 30
+        time_step = 0.1
+        rospy.init_node('vehicle_dynamics')
+        vd = VehicleDynamics(m=2000, mu=0.3, g=9.8, area=10, c=0.7, rho=1.3, v0=20, p0=0, noise_mag=0)
+        rospy.Subscriber("control_input", Float32, vd.update_control)
+        pub_xd = rospy.Publisher("velocity", Float32, queue_size=10)
+        pub_x = rospy.Publisher("position", Float32, queue_size=10)
+        pub_d = rospy.Publisher("desired", Float32, queue_size=10)
+        pub_error = rospy.Publisher("error", Float32, queue_size=10)
+        
+        rate = rospy.Rate(0.5)
+        # wait until param says controller is ready
+        while not rospy.is_shutdown():
+            if rospy.has_param("controller_ready"):
+                if rospy.get_param("controller_ready") == "ready":
+                    break
+            rospy.logwarn("Waiting for controller_ready to be 'ready'")
+            rate.sleep()
+        
+        # wait until param says graph is ready
+        while not rospy.is_shutdown():
+            if rospy.has_param("graph_ready"):
+                if rospy.get_param("graph_ready") == "ready":
+                    break
+            rospy.logwarn("Waiting for graph_ready to be 'ready'")
+            rate.sleep()
+        
+        rospy.logwarn("Starting dynamics")
+        rate = rospy.Rate(1.0/time_step)
+        # run vehicle dynamics for ~10 seconds
+        start_time = rospy.get_time()
+        updated_desired = False
+        while not rospy.is_shutdown():
+            time_elapsed = rospy.get_time() - start_time
+            if not updated_desired and time_elapsed > 15:
+                updated_desired = True
+                desired += 30
+                
+            # quit after 30 sec of running
+            if time_elapsed > 30:
+                rospy.set_param("controller_ready", "false")
+                rospy.logwarn("Time limit reached -- stopping dynamics")
+                exit()
+            vd.iterate(time_step)
+            pub_xd.publish(vd.xd)
+            pub_x.publish(vd.x)
+            pub_d.publish(desired)
+            pub_error.publish(desired - vd.x)
+            #rospy.logwarn("v=%f, x=%f, e=%f" % (vd.xd, vd.x, desired-vd.x))
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
---
+++ eece5560/packages/odometry_hw/src/wheel_tick_pub.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import csv
+import rospy
+from odometry_hw.msg import DistWheel
+
+def pattern_generator(i):
+    if i < 2:
+        return (0,0.0785) # left
+    elif i >= 2 and i < 22:
+        return (.1,.1) # up before U
+    elif i >= 22 and i < 24:
+        return (0.0785,0) # right
+    elif i >= 24 and i < 25:
+        return (0.1,0.1) # space before U
+    elif i >= 25 and i < 27:
+        return (0.0785,0) # right
+    elif i >= 27 and i < 47:
+        return (.1,.1) # down of U
+    elif i >= 47 and i < 49:
+        return (0,0.0785) # left
+    elif i >= 49 and i < 60:
+        return (.1,.1) # bottom of U
+    elif i >= 60 and i < 62:
+        return (0,0.0785) # left
+    elif i >= 62 and i < 82:
+        return (.1,.1) # up of U/M
+    elif i >= 82 and i < 85:
+        return (0.0785,0) # right 135
+    elif i >= 85 and i < 95:
+        return (.1,.1) # down diagonal
+    elif i >= 95 and i < 97:
+        return (0,0.0785) # left
+    elif i >= 97 and i < 107:
+        return (.1,.1) # up diagonal
+    elif i >= 107 and i < 110:
+        return (0.0785,0) # right 135
+    elif i >= 110 and i < 130:
+        return (.1,.1) # down for M/L    
+    elif i >= 130 and i < 132:
+        return (0,0.0785) # left
+    elif i >= 132 and i < 145:
+        return (.1,.1) # bottom of L
+        
+    return (0,0)
+
+if __name__ == "__main__":
+    rospy.init_node('wheel_tick_pub', anonymous=True)
+    pub = rospy.Publisher("dist_wheel", DistWheel, queue_size=10)
+    rate = rospy.Rate(10) # 10hz
+
+    while not rospy.is_shutdown():
+        if rospy.has_param("odom_ready"):
+            if rospy.get_param("odom_ready") == "ready":
+                break
+        rospy.logwarn("Waiting for odom_ready to be 'ready'")
+        rate.sleep()
+
+    for i in range(10):
+        pub.publish(DistWheel(0,0))
+        if rospy.is_shutdown():
+            break
+        rate.sleep()
+
+    for i in range(160):
+        ticks_left,ticks_right = pattern_generator(i)
+        rospy.logwarn("left: %f right: %f" % (ticks_left,ticks_right))
+        ticks = DistWheel()
+        ticks.dist_wheel_left = ticks_left
+        ticks.dist_wheel_right = ticks_right
+        pub.publish(ticks)
+        if rospy.is_shutdown():
+            break
+        rate.sleep()
+    
